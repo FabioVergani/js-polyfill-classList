998a5c100cbc184bf2c547e455271427c25d1b8e
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-23 20:14:07 +0100

    Update classList-shim.js

--- classList-shim.js
+++ classList-shim.js
@@ -1,6 +1,6 @@
 //#polyfills by feature-detect
 
-//Prerequisiti: array.indexOf +
+//Prerequisiti:
 'use strict';
 var SP=String.prototype;
 SP.trim=SP.trim||(function(p){return function(){return this.replace(p,'');};})(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g);
